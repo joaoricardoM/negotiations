88e3a0304b2b360fcd17fe4a52b225bf3f647ad1
joaoRicardo4357 <joao_pia_mds@hotmail.com>
2022-04-26 12:15:39 -0300

    add type

---
+++ app/app.ts
@@ -0,0 +1,4 @@
+import { Negociacao } from "../dist/js/models/negociacao.js"
+
+const negociacao = new Negociacao (new Date());
+console.log(negociacao.volume);
--- dist/js/app.js
+++ dist/js/app.js
@@ -1,4 +1,3 @@
-import { Negociacao } from "./models/negociacao.js";
-
-const negociacao = new Negociacao (new Date(), 10, 100);
-console.log(negociacao.data);
+import { Negociacao } from "../dist/js/models/negociacao.js";
+const negociacao = new Negociacao(new Date());
+console.log(negociacao.volume);
