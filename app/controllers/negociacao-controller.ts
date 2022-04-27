4d21eb68260a1c39531585ea2d3a89344208fd7d
joaoRicardo4357 <joao_pia_mds@hotmail.com>
2022-04-27 10:45:38 -0300

    update

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -8,7 +8,7 @@ export class negociacaoController {
     constructor(){
         this.inputData = document.querySelector('#data');
         this.inputQuantidade = document.querySelector('#quantidade');
-        this.inputValor = document.querySelector('#valor')
+        this.inputValor = document.querySelector('#valor');
     }
 
     adiciona(): void{
