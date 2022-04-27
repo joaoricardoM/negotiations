83233126be416f1c5030c31610e78b7b124afc9f
joaoRicardo4357 <joao_pia_mds@hotmail.com>
2022-04-27 10:07:14 -0300

    update base

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,9 +1,9 @@
 export class Negociacao {
-    private _data;
-    private _quantidade;
-    private _valor;
+    private _data: Date;
+    private _quantidade: number;
+    private _valor: number;
 
-    constructor(data, valor, quantidade) {
+    constructor(data: Date, valor: number, quantidade: number ) {
         this._data = data;
         this._quantidade = quantidade;
         this._valor = valor;
