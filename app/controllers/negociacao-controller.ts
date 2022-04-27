61380abe27cdfe9fb50e32f3ff782c92e49d99c7
joaoRicardo4357 <joao_pia_mds@hotmail.com>
2022-04-27 14:20:09 -0300

    update project

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -1,23 +1,26 @@
 import { Negociacao } from "../models/negociacao.js";
+import { Negociacoes } from "../models/negociacoes.js";
 
 export class negociacaoController {
     private inputData: HTMLInputElement;
     private inputQuantidade: HTMLInputElement;
     private inputValor: HTMLInputElement;
+    private negociacoes = new Negociacoes;
 
-    constructor(){
+    constructor() {
         this.inputData = document.querySelector('#data');
         this.inputQuantidade = document.querySelector('#quantidade');
         this.inputValor = document.querySelector('#valor');
     }
 
-    adiciona(): void{
+    adiciona(): void {
         const negociacao = this.criarNegociacao();
-        console.log(negociacao);
+        this.negociacoes.adiciona(negociacao);
+        console.log(this.negociacoes.lista());
         this.limparFormulario();
     }
 
-    criarNegociacao(): Negociacao{
+    criarNegociacao(): Negociacao {
         const exp = /-/g;
         const data = new Date(this.inputData.value.replace(exp, ","));
         const quantidade = parseInt(this.inputQuantidade.value);
@@ -25,7 +28,7 @@ export class negociacaoController {
         return new Negociacao(data, quantidade, valor);
     }
 
-    limparFormulario(): void{
+    limparFormulario(): void {
         this.inputData.value = '';
         this.inputQuantidade.value = '';
         this.inputValor.value = '';
