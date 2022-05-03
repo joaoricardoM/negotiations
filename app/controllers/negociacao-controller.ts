1bee9b5950b9d7db98a4d01eb132ed0a1a07d182
joaoRicardo4357 <joao_pia_mds@hotmail.com>
2022-05-03 16:20:44 -0300

    update project

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -1,3 +1,4 @@
+import { DiasSemana } from "../enums/dias-semana.js"
 import { Negociacao } from "../models/negociacao.js";
 import { Negociacoes } from "../models/negociacoes.js";
 import { MensagemView } from "../views/mensagem-view.js";
@@ -18,26 +19,35 @@ export class negociacaoController {
         this.negociacoesView.update(this.negociacoes);
     }
 
-    adiciona(): void {
-        const negociacao = this.criarNegociacao();
-        this.negociacoes.adiciona(negociacao);
-        this.negociacoesView.update(this.negociacoes);
-        this.mensagemView.update('Negociacao enviada com sucessso');
-        this.limparFormulario();
+    public adiciona(): void {
+        const negociacao = Negociacao.criaDe(
+            this.inputData.value,
+            this.inputQuantidade.value,
+            this.inputValor.value
+        );
+        if (!this.ehDiaUtil(negociacao.data)){
+            this.mensagemView.update('Apenas negociações em dias útei são aceitas')
+            return;
+        }
+
+        this.negociacoes.adiciona(negociacao)
+        this.limparFormulario()
+        this.atulizaView()
     }
 
-    criarNegociacao(): Negociacao {
-        const exp = /-/g;
-        const data = new Date(this.inputData.value.replace(exp, ","));
-        const quantidade = parseInt(this.inputQuantidade.value);
-        const valor = parseFloat(this.inputValor.value);
-        return new Negociacao(data, quantidade, valor);
+    private ehDiaUtil(data: Date){
+        return data.getDay() > DiasSemana.DOMINGO && data.getDay() < DiasSemana.SABADO
     }
 
-    limparFormulario(): void {
+    public limparFormulario(): void {
         this.inputData.value = '';
         this.inputQuantidade.value = '';
         this.inputValor.value = '';
         this.inputData.focus();
+    }
+
+    private atulizaView(): void {
+        this.negociacoesView.update(this.negociacoes);
+        this.mensagemView.update("Negociação adicionada com sucesso")
     }
 }
--- app/views/negociacoes-view.ts
+++ app/views/negociacoes-view.ts
@@ -1,14 +1,8 @@
 import { Negociacoes } from '../models/negociacoes.js';
+import { View } from './view';
+export class NegociacoesView extends View <Negociacoes> {
 
-export class NegociacoesView {
-
-    private elemento: HTMLElement;
-
-    constructor(seletor: string) {
-        this.elemento = document.querySelector(seletor);
-    }
-
-    template(model: Negociacoes): string {
+     protected template(model: Negociacoes): string {
         return `
         <table class="table table-hover table-bordered">
             <thead>
@@ -22,7 +16,7 @@ export class NegociacoesView {
                 ${model.lista().map(negociacao => {
                     return `
                         <tr>
-                            <td>${new Intl.DateTimeFormat().format(negociacao.data)}</td>
+                            <td>${this.formatar(negociacao.data)}</td>
                             <td>${negociacao.quantidade}</td>
                             <td>${negociacao.valor}</td>
                         </tr>
@@ -33,9 +27,8 @@ export class NegociacoesView {
         `;
     }
 
-    update(model: Negociacoes): void {
-        const template = this.template(model);
-        console.log(template);
-        this.elemento.innerHTML = template;
+    private formatar( data : Date): string {
+        return new Intl.DateTimeFormat()
+            .format(data);
     }
 }
