ea7ead9133460ebff00bfba3dc710d447c04b1fa
joaoRicardo4357 <joao_pia_mds@hotmail.com>
2022-05-03 12:16:23 -0300

    create viewmodel

---
+++ app/views/view.ts
@@ -0,0 +1,22 @@
+export abstract class View<T>{
+
+    protected element: HTMLElement;
+    private escapar = false;
+
+    constructor(seletor: string, escapar?: boolean) {
+        this.element = document.querySelector(seletor);
+        if (escapar) {
+            this.escapar = escapar;
+        }
+    }
+
+    public update (model: T): void {
+        let template = this.template(model);
+            if(this.escapar) {
+                template = template
+                .replace(/<script>[\s\S]*?<\/script>/,'');
+            }
+            this.element.innerHTML = template;
+    }
+        protected abstract template (model: T): string;
+}
