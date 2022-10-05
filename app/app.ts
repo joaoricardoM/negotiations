08fb37b173146ad53d3160725e64779b7f5cd314
joaoRicardo4357 <joao_pia_mds@hotmail.com>
2022-10-05 16:52:35 -0300

    remove from home

--- app/app.ts
+++
@@ -1,8 +0,0 @@
-import { negociacaoController } from './controllers/negociacao-controller.js';
-
-const controller = new negociacaoController();
-const form = document.querySelector('.form');
-form.addEventListener('submit', event => {
-    event.preventDefault();
-    controller.adiciona();
-});
